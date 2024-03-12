cd7444aacc88869eb13048e9353145d45f641cff
isaacfkessler <kesslerisaack@gmail.com>
2024-03-11 22:02:53 -0300

    fix: connection refuse mongodb, reordering structure

--- app.py
+++ app.py
@@ -4,18 +4,28 @@ from flask_mongoengine import MongoEngine
 
 
 app = Flask(__name__)
-api = Api(app)
-db = MongoEngine(app)
 
 app.config["MONGODB_SETTINGS"] = [
     {
         "db": "users",
         "host": "mongodb",
         "port": 27017,
-        "user" : "admin",
-        "password" : "isaacdevops"
+        "username": "admin",
+        "password": "isaacdevops"
     }
 ]
+
+api = Api(app)
+db = MongoEngine(app)
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.StringField(required=True)
+    first_name = db.EmailField(required=True)
+    last_name = db.StringField(required=True)
+    birth_date = db.DateTimeField(required=True)
+
 
 class Users(Resource):
     def get(self):
