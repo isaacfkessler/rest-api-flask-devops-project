1bb0320b2b0d572b35196c24b5896c003ba6135f
isaacfkessler <kesslerisaack@gmail.com>
2024-03-11 21:29:43 -0300

    feat: add mongoengine

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine(app)
 
+app.config["MONGODB_SETTINGS"] = [
+    {
+        "db": "users",
+        "host": "mongodb",
+        "port": 27017,
+        "user" : "admin",
+        "password" : "isaacdevops"
+    }
+]
 
 class Users(Resource):
     def get(self):
